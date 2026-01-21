fe7d46a45c296eba09584e9d8f6dc30a05238199
awanapp <awan.app.eg@gmail.com>
2026-01-21 13:37:48 +0200

    Remove dist folder from git tracking

--- dist/awan/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-WCLCBI7J.mjs";import"./chunk-OODUHXL5.mjs";import"./chunk-J4BDXTMW.mjs";import"./chunk-XAOZC3E3.mjs";import"./chunk-XZAMSGQ2.mjs";import"./chunk-4JYF2G4G.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
--- dist/awan/server/render-utils.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{c as o,d as r,e as n}from"./chunk-OODUHXL5.mjs";import"./chunk-J4BDXTMW.mjs";import{ob as e}from"./chunk-4JYF2G4G.mjs";import"./chunk-DCFYTP2A.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
